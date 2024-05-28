08555e0355d8d95b12b04bc901c40d20f93207fc
Daniel Schömer <daniel.schoemer@gmx.net>
2024-05-28 17:33:53 +0200

    Move zmk build directory to ./_build and copy firmware files to ./_firmware

--- build.sh
+++ build.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
 
-if [[ ! -f /.dockerenv ]] ; then
-  exec docker run --rm -it -v .:/app -v ./work:/work zmkfirmware/zmk-build-arm:stable "/app/$(basename "$0")"
-fi
-
 set -o pipefail
 
 _main() {
+  _exec_in_container
+
   cd "$(dirname "$0")" || exit 1
 
   project_dir="$(realpath .)"
-  base_dir="/work/zmk-config"
+  base_dir="/_build/zmk-config"
   config_path="config"
   archive_name="firmware"
   zmk_load_arg=" -DZMK_EXTRA_MODULES='${project_dir}'"
@@ -27,7 +25,7 @@ _main() {
   _west_compile "${base_dir}" "${config_path}" nice_nano_v2 kyria_rev3_right
 
   echo
-  ls -lh /work/artifacts/
+  ls -lh /_firmware
 }
 
 _west_prepare() {
@@ -45,12 +43,11 @@ _west_compile() {
   local board="$3"
   local shield="$4"
 
-  local build_dir="/work/${board}-${shield}"
-  #local zephyr_version="${ZEPHYR_VERSION}"
+  local build_dir="$(realpath "${base_dir}/../${board}-${shield}")"
+  local artifacts_dir="/_firmware"
   local extra_cmake_args="-DSHIELD='${shield}'${zmk_load_arg}"
   local display_name="${shield} - ${board}"
-  local artifact_name="${shield}-${board}-zmk"
-  local artifacts_dir="$(realpath "${build_dir}/../artifacts")"
+  local artifact_name="${shield}-${board}-zmk.$(date +%F_%H%M%S)"
 
   mkdir -pv "${build_dir}" "${artifacts_dir}"
   west build -s zmk/app -d "${build_dir}" -b "${board}" -- -DZMK_CONFIG="${base_dir}/${config_path}" ${extra_cmake_args} ${cmake_args}
@@ -58,6 +55,30 @@ _west_compile() {
   cp "${build_dir}/zephyr/.config" "${artifacts_dir}/${artifact_name}.Kconfig"
   cp "${build_dir}/zephyr/zephyr.dts" "${artifacts_dir}/${artifact_name}.zephyr.dts"
   cp "${build_dir}/zephyr/zmk.uf2" "${artifacts_dir}/${artifact_name}.uf2"
+}
+
+_start_container_runtime() {
+  for RUNTIME in orbctl /Applications/{OrbStack,Docker}.app ; do
+    command -v "${RUNTIME}" && break
+    [[ -d "${RUNTIME}" ]] && break
+  done
+
+  case "${RUNTIME}" in
+    *.app )
+      open -a "${RUNTIME}"
+      ;;
+    * )
+      "${RUNTIME}" start
+      ;;
+  esac
+}
+
+_exec_in_container() {
+  if [[ ! -f /.dockerenv ]] ; then
+    _start_container_runtime
+    exec docker run --rm -it -v .:/app -v ./_build:/_build -v ./_firmware:/_firmware zmkfirmware/zmk-build-arm:stable "/app/$(basename "$0")"
+    exit
+  fi
 }
 
 _main "${@}"
